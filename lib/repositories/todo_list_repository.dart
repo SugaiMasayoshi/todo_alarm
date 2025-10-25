aab609872a93429f73ec215c251990a2a317f83a
Block <203180274+SugaiMasayoshi@users.noreply.github.com>
2025-10-25 19:17:51 +0900

    refactor: Todo操作をRepositoryからViewModelに移動 (#5)

--- lib/repositories/todo_list_repository.dart
+++ lib/repositories/todo_list_repository.dart
@@ -2,12 +2,11 @@ import 'dart:convert';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:todo_alarm/repositories/local_storage_repository.dart';
 import 'package:todo_alarm/repositories/models/todo_list_model.dart';
-import 'models/todo_item_model.dart';
 
-part '../generated/repositories/todo_repository.g.dart';
+part '../generated/repositories/todo_list_repository.g.dart';
 
 @riverpod
-class TodoRepository extends _$TodoRepository {
+class TodoListRepository extends _$TodoListRepository {
   static const String _todoListKey = 'todo_list';
 
   @override
@@ -21,29 +20,11 @@ class TodoRepository extends _$TodoRepository {
     return TodoListModel.fromJson(jsonMap);
   }
 
-  Future<void> _saveTodoList(TodoListModel todoList) async {
+  Future<void> save(TodoListModel todoList) async {
     final storage = ref.read(localStorageRepositoryProvider);
     final jsonMap = todoList.toJson();
     final jsonString = jsonEncode(jsonMap);
     await storage.setString(_todoListKey, jsonString);
     state = todoList;
-  }
-
-  Future<void> addTodo(TodoItemModel todo) async {
-    await _saveTodoList(state.copyWith(items: {...state.items, todo.id: todo}));
-  }
-
-  Future<void> updateTodo(TodoItemModel updatedTodo) async {
-    final newItems = Map<String, TodoItemModel>.from(state.items);
-    if (newItems.containsKey(updatedTodo.id)) {
-      newItems[updatedTodo.id] = updatedTodo;
-    }
-    await _saveTodoList(state.copyWith(items: newItems));
-  }
-
-  Future<void> deleteTodo(String id) async {
-    final newItems = Map<String, TodoItemModel>.from(state.items);
-    newItems.remove(id);
-    await _saveTodoList(state.copyWith(items: newItems));
   }
 }
--- lib/ui/todo_list/todo_list_view_model.dart
+++ lib/ui/todo_list/todo_list_view_model.dart
@@ -1,7 +1,7 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:todo_alarm/repositories/models/todo_item_model.dart';
 import 'package:todo_alarm/repositories/models/todo_list_model.dart';
-import 'package:todo_alarm/repositories/todo_repository.dart';
+import 'package:todo_alarm/repositories/todo_list_repository.dart';
 import 'package:uuid/uuid.dart';
 
 part '../../generated/ui/todo_list/todo_list_view_model.g.dart';
@@ -10,24 +10,38 @@ part '../../generated/ui/todo_list/todo_list_view_model.g.dart';
 class TodoListViewModel extends _$TodoListViewModel {
   @override
   TodoListModel build() {
-    final todoListState = ref.watch(todoRepositoryProvider);
+    final todoListState = ref.watch(todoListRepositoryProvider);
     return todoListState;
   }
 
-  Future<void> addTodo({required String title}) async {
+  Future<void> addTodo(String title) async {
     final todo = TodoItemModel(
       id: const Uuid().v4(),
       title: title,
       status: TodoStatus.todo,
     );
-    await ref.read(todoRepositoryProvider.notifier).addTodo(todo);
+
+    final newItems = Map<String, TodoItemModel>.from(state.items)
+      ..[todo.id] = todo;
+
+    await ref
+        .read(todoListRepositoryProvider.notifier)
+        .save(state.copyWith(items: newItems));
   }
 
+  // 存在していないTodoを更新しようとした場合は追加されます
   Future<void> updateTodo(TodoItemModel updatedTodo) async {
-    await ref.read(todoRepositoryProvider.notifier).updateTodo(updatedTodo);
+    final newItems = Map<String, TodoItemModel>.from(state.items)
+      ..[updatedTodo.id] = updatedTodo;
+
+    await ref
+        .read(todoListRepositoryProvider.notifier)
+        .save(state.copyWith(items: newItems));
   }
 
   Future<void> deleteTodo(String id) async {
-    await ref.read(todoRepositoryProvider.notifier).deleteTodo(id);
+    await ref
+        .read(todoListRepositoryProvider.notifier)
+        .save(state.copyWith(items: {...state.items}..remove(id)));
   }
 }
