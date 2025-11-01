e938bf77d767245e3808070fb21aaf5177c43dfd
SugaiMasayoshi <203180274+SugaiMasayoshi@users.noreply.github.com>
2025-11-01 19:19:07 +0900

    fix: 生成ファイルのパスを更新

--- lib/ui/todo_item/todo_item_provider.dart
+++ lib/ui/todo_item/todo_item_provider.dart
@@ -3,7 +3,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:todo_alarm/repositories/models/todo_item_model.dart';
 import 'package:todo_alarm/ui/todo_list/todo_list_view_model.dart';
 
-part '../../generated/ui/todo_list/todo_item_provider.g.dart';
+part '../../generated/ui/todo_item/todo_item_provider.g.dart';
 
 @riverpod
 TodoItemModel? todoItem(Ref ref, String id) {
